ee1da665c5f4feb65dd09931e44fa9354e85fc45
alessandrolongo005-max <alessandrolongo005@gmail.com>
2026-07-14 12:21:14 +0200

    Update server.py

--- server.py
+++ server.py
@@ -249,4 +249,5 @@ app.include_router(api, prefix="/api")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
